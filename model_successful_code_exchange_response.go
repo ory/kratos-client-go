e9aeb0ece9b90a58659db5287a7d4b895d493209
aeneasr <3372410+aeneasr@users.noreply.github.com>
2023-08-31 08:41:50 +0000

    autogen: regenerate OpenAPI client for v1.0.0
    Version: v1.0.0

--- model_successful_code_exchange_response.go
+++ model_successful_code_exchange_response.go
@@ -15,36 +15,36 @@ import (
 	"encoding/json"
 )
 
-// SuccessfulNativeLogin The Response for Login Flows via API
-type SuccessfulNativeLogin struct {
+// SuccessfulCodeExchangeResponse The Response for Registration Flows via API
+type SuccessfulCodeExchangeResponse struct {
 	Session Session `json:"session"`
 	// The Session Token  A session token is equivalent to a session cookie, but it can be sent in the HTTP Authorization Header:  Authorization: bearer ${session-token}  The session token is only issued for API flows, not for Browser flows!
 	SessionToken *string `json:"session_token,omitempty"`
 	AdditionalProperties map[string]interface{}
 }
 
-type _SuccessfulNativeLogin SuccessfulNativeLogin
+type _SuccessfulCodeExchangeResponse SuccessfulCodeExchangeResponse
 
-// NewSuccessfulNativeLogin instantiates a new SuccessfulNativeLogin object
+// NewSuccessfulCodeExchangeResponse instantiates a new SuccessfulCodeExchangeResponse object
 // This constructor will assign default values to properties that have it defined,
 // and makes sure properties required by API are set, but the set of arguments
 // will change when the set of required properties is changed
-func NewSuccessfulNativeLogin(session Session) *SuccessfulNativeLogin {
-	this := SuccessfulNativeLogin{}
+func NewSuccessfulCodeExchangeResponse(session Session) *SuccessfulCodeExchangeResponse {
+	this := SuccessfulCodeExchangeResponse{}
 	this.Session = session
 	return &this
 }
 
-// NewSuccessfulNativeLoginWithDefaults instantiates a new SuccessfulNativeLogin object
+// NewSuccessfulCodeExchangeResponseWithDefaults instantiates a new SuccessfulCodeExchangeResponse object
 // This constructor will only assign default values to properties that have it defined,
 // but it doesn't guarantee that properties required by API are set
-func NewSuccessfulNativeLoginWithDefaults() *SuccessfulNativeLogin {
-	this := SuccessfulNativeLogin{}
+func NewSuccessfulCodeExchangeResponseWithDefaults() *SuccessfulCodeExchangeResponse {
+	this := SuccessfulCodeExchangeResponse{}
 	return &this
 }
 
 // GetSession returns the Session field value
-func (o *SuccessfulNativeLogin) GetSession() Session {
+func (o *SuccessfulCodeExchangeResponse) GetSession() Session {
 	if o == nil {
 		var ret Session
 		return ret
@@ -55,7 +55,7 @@ func (o *SuccessfulNativeLogin) GetSession() Session {
 
 // GetSessionOk returns a tuple with the Session field value
 // and a boolean to check if the value has been set.
-func (o *SuccessfulNativeLogin) GetSessionOk() (*Session, bool) {
+func (o *SuccessfulCodeExchangeResponse) GetSessionOk() (*Session, bool) {
 	if o == nil {
 		return nil, false
 	}
@@ -63,12 +63,12 @@ func (o *SuccessfulNativeLogin) GetSessionOk() (*Session, bool) {
 }
 
 // SetSession sets field value
-func (o *SuccessfulNativeLogin) SetSession(v Session) {
+func (o *SuccessfulCodeExchangeResponse) SetSession(v Session) {
 	o.Session = v
 }
 
 // GetSessionToken returns the SessionToken field value if set, zero value otherwise.
-func (o *SuccessfulNativeLogin) GetSessionToken() string {
+func (o *SuccessfulCodeExchangeResponse) GetSessionToken() string {
 	if o == nil || o.SessionToken == nil {
 		var ret string
 		return ret
@@ -78,7 +78,7 @@ func (o *SuccessfulNativeLogin) GetSessionToken() string {
 
 // GetSessionTokenOk returns a tuple with the SessionToken field value if set, nil otherwise
 // and a boolean to check if the value has been set.
-func (o *SuccessfulNativeLogin) GetSessionTokenOk() (*string, bool) {
+func (o *SuccessfulCodeExchangeResponse) GetSessionTokenOk() (*string, bool) {
 	if o == nil || o.SessionToken == nil {
 		return nil, false
 	}
@@ -86,7 +86,7 @@ func (o *SuccessfulNativeLogin) GetSessionTokenOk() (*string, bool) {
 }
 
 // HasSessionToken returns a boolean if a field has been set.
-func (o *SuccessfulNativeLogin) HasSessionToken() bool {
+func (o *SuccessfulCodeExchangeResponse) HasSessionToken() bool {
 	if o != nil && o.SessionToken != nil {
 		return true
 	}
@@ -95,11 +95,11 @@ func (o *SuccessfulNativeLogin) HasSessionToken() bool {
 }
 
 // SetSessionToken gets a reference to the given string and assigns it to the SessionToken field.
-func (o *SuccessfulNativeLogin) SetSessionToken(v string) {
+func (o *SuccessfulCodeExchangeResponse) SetSessionToken(v string) {
 	o.SessionToken = &v
 }
 
-func (o SuccessfulNativeLogin) MarshalJSON() ([]byte, error) {
+func (o SuccessfulCodeExchangeResponse) MarshalJSON() ([]byte, error) {
 	toSerialize := map[string]interface{}{}
 	if true {
 		toSerialize["session"] = o.Session
@@ -115,11 +115,11 @@ func (o SuccessfulNativeLogin) MarshalJSON() ([]byte, error) {
 	return json.Marshal(toSerialize)
 }
 
-func (o *SuccessfulNativeLogin) UnmarshalJSON(bytes []byte) (err error) {
-	varSuccessfulNativeLogin := _SuccessfulNativeLogin{}
+func (o *SuccessfulCodeExchangeResponse) UnmarshalJSON(bytes []byte) (err error) {
+	varSuccessfulCodeExchangeResponse := _SuccessfulCodeExchangeResponse{}
 
-	if err = json.Unmarshal(bytes, &varSuccessfulNativeLogin); err == nil {
-		*o = SuccessfulNativeLogin(varSuccessfulNativeLogin)
+	if err = json.Unmarshal(bytes, &varSuccessfulCodeExchangeResponse); err == nil {
+		*o = SuccessfulCodeExchangeResponse(varSuccessfulCodeExchangeResponse)
 	}
 
 	additionalProperties := make(map[string]interface{})
@@ -133,38 +133,38 @@ func (o *SuccessfulNativeLogin) UnmarshalJSON(bytes []byte) (err error) {
 	return err
 }
 
-type NullableSuccessfulNativeLogin struct {
-	value *SuccessfulNativeLogin
+type NullableSuccessfulCodeExchangeResponse struct {
+	value *SuccessfulCodeExchangeResponse
 	isSet bool
 }
 
-func (v NullableSuccessfulNativeLogin) Get() *SuccessfulNativeLogin {
+func (v NullableSuccessfulCodeExchangeResponse) Get() *SuccessfulCodeExchangeResponse {
 	return v.value
 }
 
-func (v *NullableSuccessfulNativeLogin) Set(val *SuccessfulNativeLogin) {
+func (v *NullableSuccessfulCodeExchangeResponse) Set(val *SuccessfulCodeExchangeResponse) {
 	v.value = val
 	v.isSet = true
 }
 
-func (v NullableSuccessfulNativeLogin) IsSet() bool {
+func (v NullableSuccessfulCodeExchangeResponse) IsSet() bool {
 	return v.isSet
 }
 
-func (v *NullableSuccessfulNativeLogin) Unset() {
+func (v *NullableSuccessfulCodeExchangeResponse) Unset() {
 	v.value = nil
 	v.isSet = false
 }
 
-func NewNullableSuccessfulNativeLogin(val *SuccessfulNativeLogin) *NullableSuccessfulNativeLogin {
-	return &NullableSuccessfulNativeLogin{value: val, isSet: true}
+func NewNullableSuccessfulCodeExchangeResponse(val *SuccessfulCodeExchangeResponse) *NullableSuccessfulCodeExchangeResponse {
+	return &NullableSuccessfulCodeExchangeResponse{value: val, isSet: true}
 }
 
-func (v NullableSuccessfulNativeLogin) MarshalJSON() ([]byte, error) {
+func (v NullableSuccessfulCodeExchangeResponse) MarshalJSON() ([]byte, error) {
 	return json.Marshal(v.value)
 }
 
-func (v *NullableSuccessfulNativeLogin) UnmarshalJSON(src []byte) error {
+func (v *NullableSuccessfulCodeExchangeResponse) UnmarshalJSON(src []byte) error {
 	v.isSet = true
 	return json.Unmarshal(src, &v.value)
 }
